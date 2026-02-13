5fd50ecd6484b7cbeb3473dd09bd5d405264068f
Vinícius Berger <viniciush4@gmail.com>
2026-02-13 10:43:32 +0000

    Remove comentário

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,4 @@
 test("GET to /api/v1/status should return 200", async () => {
-  // expect(calculadora.somar(2, 2)).toBe(4);
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
