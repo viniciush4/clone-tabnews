b8a2aab354d04aafbb0f0e3cdebee02cbf3bca18
Vinícius Berger <viniciush4@gmail.com>
2026-03-22 17:08:04 +0000

    Finaliza o endpoint `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,13 +1,39 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
+  // Data e hora
   const updatedAt = new Date().toISOString();
 
-  response.status(200).json({ updated_at: updatedAt });
-
   // Versão do PostgreSQL
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
   // Conexões máximas
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
   // Conexões ativas
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: parseInt(databaseOpenedConnectionsValue),
+      },
+    },
+  });
 }
 
 export default status;
